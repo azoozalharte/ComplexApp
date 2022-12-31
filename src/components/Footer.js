e1fb0e0093c9aab3eaedbcd3429de7b71e1bab4d
azoozalharte <zezo50115@protonmail.com>
2022-12-31 18:49:33 +0300

    lets start

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+export default function Footer() {
+  return (
+    <div>
+      <footer className="border-top text-center small text-muted py-3">
+        <p>
+          <a href="/" className="mx-1">
+            Home
+          </a>{" "}
+          |
+          <a className="mx-1" href="/about-us">
+            About Us
+          </a>{" "}
+          |
+          <a className="mx-1" href="/terms">
+            Terms
+          </a>
+        </p>
+        <p className="m-0">
+          Copyright &copy; 2020{" "}
+          <a href="/" className="text-muted">
+            ComplexApp
+          </a>
+          . All rights reserved.
+        </p>
+      </footer>
+    </div>
+  );
+}
